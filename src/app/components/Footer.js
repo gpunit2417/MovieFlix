10c091ee963d98d6ac8a7260acceb6ac4320c4cb
gpunit2417 <punitgoyal106@gmail.com>
2025-02-01 22:19:41 +0530

    footer logo name change

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
                     <div className={footerStyles.top}>
                         <div className={footerStyles['logo-details']}>
                             {/*<i className={footerStyles.fab fa-slack]></i>*/}
-                            <span className={footerStyles.logo_name}>Thapa Technical</span>
+                            <span className={footerStyles.logo_name}>Punit Goyal</span>
                         </div>
                         <div className={footerStyles[`media-icons`]}>
                             <Link href="https://www.facebook.com/punit.goyal.904/"><i > <FaFacebookF/> </i></Link>
